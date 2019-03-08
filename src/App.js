ae5da1be0338b0fdfe76743cd1d2e4957b233ba5
Samson <983521365@qq.com>
2019-03-08 15:01:45 +0800

    modify package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'  // 子元素只能一个div包裹
-import { HashRouter , Route} from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom'
 import { GlobalStyle } from './style.js'
 import { GlobalStyleIcon } from './statics/iconFont/iconfont'
 import Header from './common/header'
@@ -19,7 +19,7 @@ class App extends Component {
         <GlobalStyleIcon />
         <Provider store = { store }
           >
-            <HashRouter>
+            <BrowserRouter>
               <div>
                 <Header></Header>
                 <Route path='/' exact component={ Home } ></Route>
@@ -27,7 +27,7 @@ class App extends Component {
                 <Route path='/write' exact component={ Write } ></Route>
                 <Route path='/detail/:id' exact component={ Detail } ></Route>
               </div>
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
       </div>
     );
